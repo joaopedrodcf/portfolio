d846776f1f131cde4f52abb18a0fe9f4e57902d4
joaopedrodcf <joaoferr93@gmail.com>
2019-07-08 01:58:29 +0100

    chore: Improve versions of packages

---
+++ src/store/genericWrapperComponent.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import memoize from 'fast-memoize';
+
+// eslint-disable-next-line func-names
+const getProps = function(state, context) {
+    return { ...state, ...context };
+};
+
+const memoizedGetProps = memoize(getProps);
+
+const genericWrapperComponent = (name, ComponentContext, Element) => {
+    function WrapperComponent(props) {
+        return (
+            <ComponentContext.Consumer>
+                {state => (
+                    <Element
+                        {...props}
+                        context={memoizedGetProps(state, props.context)}
+                    />
+                )}
+            </ComponentContext.Consumer>
+        );
+    }
+
+    WrapperComponent.displayName = name;
+    return WrapperComponent;
+};
+
+export default genericWrapperComponent;
